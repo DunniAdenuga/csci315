024403e1d1e1315a1e0ee51625fd427bd670bce5
Dunni Adenuga <ia005@bucknell.edu>
2015-11-13 14:10:27 -0500

    Lab 10.1 completed

---
+++ Labs/Lab10/ishell.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <string.h>
+extern char **environ;
+
+char* command;
+int count;
+char* tokenizer(void);
+int main(){
+  
+  pid_t pid;
+  int status;
+  char *argvs[50];
+  while(1){
+    //printf("ishell> ");
+    command = readline("ishell> ");
+
+    if((pid = fork()) == -1){
+      return (0);
+    }
+    if(pid > 0)//parent process
+      {
+	wait(&status);
+  }
+    else//child process
+      {
+	count = 0;
+	//char *token1 = tokenizer();
+	char *file = tokenizer();
+	int i = 1;
+	
+	char *token = tokenizer();
+	while( token != NULL){
+	  argvs[i] = token;
+	  i++;
+	  token = tokenizer();
+	}
+	argvs[i] = NULL;
+	execvp(file, argvs);
+    }
+}
+  return 0;
+}
+
+
+
+
+char *tokenizer(void){
+  
+  const char space[2] = " ";
+  char *token;
+  if (count == 0){
+  token = strtok(command, space);
+  count++;
+  }
+  else{
+  token = strtok(NULL, space);
+  }
+  return token;
+  }
